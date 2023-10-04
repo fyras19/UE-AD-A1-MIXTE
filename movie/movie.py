481f62d22bcbf03748094814cacfe67636e6ba0a
Aymane <aymanjeb02@gmail.com>
2023-10-04 10:02:53 +0200

    files modified

--- movie/movie.py
+++ movie/movie.py
@@ -9,6 +9,21 @@ HOST = '0.0.0.0'
 app = Flask(__name__)
 
 # todo create elements for Ariadne
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+movie = ObjectType('Movie')
+query.set_field('movie_with_id', r.movie_with_id)
+schema = make_executable_schema(type_defs, movie, query)
+
+
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+schema = make_executable_schema(type_defs, movie, query, mutation)
+
+actor = ObjectType('Actor')
+movie.set_field('actors',r.resolve_actors_in_movie)
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
 
 # root message
@@ -19,13 +34,26 @@ def home():
 #####
 # graphql entry points
 
+
+
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
     
+
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+                        schema,
+                        data,
+                        context_value=None,
+                        debug=app.debug
+                    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
