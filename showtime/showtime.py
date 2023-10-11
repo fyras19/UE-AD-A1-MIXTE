65a8cc9a6530bc57602c413841945eeff9560a37
Firas Yahyaoui <firas.yahyaoui@imt-atlantique.net>
2023-10-11 09:25:51 +0200

    Add showtime grpc server

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -4,11 +4,24 @@ import showtime_pb2
 import showtime_pb2_grpc
 import json
 
+
 class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
 
     def __init__(self):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
+
+    def GetSchedule(self, request, context):
+        for day in self.db:
+            yield showtime_pb2.TimeSlot(date=day["date"], movies=day["movies"])
+
+    def GetMoviesByDate(self, request, context):
+        date = request.date
+        for day in self.db:
+            if day["date"] == date:
+                return showtime_pb2.TimeSlot(date=day["date"], movies=day["movies"])
+        return showtime_pb2.TimeSlot(date="", movies=[])
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
